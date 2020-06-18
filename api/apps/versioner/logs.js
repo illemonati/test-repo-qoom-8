153e53c127fcab665eafc9a0bf06e19ff6fb304d
illemonati <40131775+illemonati@users.noreply.github.com>
2020-06-18 14:46:35 -0700

    qoom-sync at Thu Jun 18 2020 21:46:21 GMT+0000 (Coordinated Universal Time)-2

---
+++ api/apps/versioner/logs.js
@@ -0,0 +1,3 @@
+HttpError: Repository creation failed.: {"resource":"Repository","code":"custom","field":"name","message":"name already exists on this account"}
+    at /app/node_modules/@octokit/request/dist-node/index.js:66:23
+    at processTicksAndRejections (internal/process/task_queues.js:97:5)
